bf528cdee14a8fd7d1f41d6930b33dfc855821d0
mcpmieda <mcpmieda@gmail.com>
2026-08-25 06:47:13 -0300

    refactor(ui): remover facade legado de separator

--- src/components/ui/separator.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Separator as HeroSeparator } from '@heroui/react';
-import { cn } from '@/lib/utils';
-
-function Separator({ className, ...props }: React.ComponentProps<typeof HeroSeparator>) {
-  return <HeroSeparator data-slot="separator" className={cn('shrink-0', className)} {...props} />;
-}
-
-export { Separator };
