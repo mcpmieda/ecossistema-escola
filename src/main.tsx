fba52b9afb845326a1fbe0a30a3cd26df5c96f1e
mcpmieda <mcpmieda@gmail.com>
2026-08-24 23:13:46 -0300

    Activate HeroUI cosmic theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 
+document.documentElement.classList.add('dark');
+document.documentElement.dataset.theme = 'ieda-cosmic';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
