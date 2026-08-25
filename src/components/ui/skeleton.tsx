786807f17166d5906fcda00aa9290508cb3d07fb
mcpmieda <mcpmieda@gmail.com>
2026-08-25 06:47:19 -0300

    refactor(ui): remover facade legado de skeleton

--- src/components/ui/skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Skeleton as HeroSkeleton } from '@heroui/react';
-import { cn } from '@/lib/utils';
-
-function Skeleton({ className, ...props }: React.ComponentProps<typeof HeroSkeleton>) {
-  return <HeroSkeleton data-slot="skeleton" className={cn('rounded-xl', className)} {...props} />;
-}
-
-export { Skeleton };
