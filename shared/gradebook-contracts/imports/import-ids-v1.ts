443c8deb3c744fac1f511c462042897f1eb66603
mcpmieda <mcpmieda@gmail.com>
2026-08-31 15:04:25 -0300

    feat(gradebook): add import contract identifiers

---
+++ shared/gradebook-contracts/imports/import-ids-v1.ts
@@ -0,0 +1,6 @@
+import type { EntityIdV1 } from '../entities';
+
+export type SourceFileManifestId = EntityIdV1<'SourceFileManifestV1'>;
+export type ImportBatchId = EntityIdV1<'ImportBatchResultV1'>;
+export type ImportFileId = EntityIdV1<'ImportBatchFileV1'>;
+export type ImportFileDiagnosticId = EntityIdV1<'ImportFileDiagnosticV1'>;
