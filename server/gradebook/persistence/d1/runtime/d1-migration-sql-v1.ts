79920cd69f1d408140e9eafea5c149ecaaae95ae
mcpmieda <mcpmieda@gmail.com>
2026-09-02 21:41:44 -0300

    feat(gradebook): load council session migration (#395)

--- server/gradebook/persistence/d1/runtime/d1-migration-sql-v1.ts
+++ server/gradebook/persistence/d1/runtime/d1-migration-sql-v1.ts
@@ -2,10 +2,12 @@ import migration0001 from '../../../../../migrations/gradebook/0001_gradebook_co
 import migration0002 from '../../../../../migrations/gradebook/0002_gradebook_records_audit_v1.sql';
 import migration0003 from '../../../../../migrations/gradebook/0003_logical_source_record_catalog_v1.sql';
 import migration0004 from '../../../../../migrations/gradebook/0004_bulletin_council_durability_v1.sql';
+import migration0005 from '../../../../../migrations/gradebook/0005_council_session_durability_v2.sql';
 
 export const GRADEBOOK_D1_MIGRATION_SQL_V1: readonly string[] = [
   migration0001,
   migration0002,
   migration0003,
   migration0004,
+  migration0005,
 ];
